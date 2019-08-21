fd98ba2fbc6f02287313278adf90537ea4b84fe2
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2019-08-21 20:34:48 +0900

    routerにdeleteを追加した

--- api/users.js
+++ api/users.js
@@ -4,8 +4,10 @@ const User = require('../model/user');
 
 const create = require('./users/create');
 const list = require('./users/list');
+const deleteCard = require('./users/delete');
 
 create({router, User, verify});
 list({router, User, verify});
+deleteCard({router, User, verify});
 
 module.exports = router;
