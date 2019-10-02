fbc853927a8afbde4c75b908e46ce7f6f03f3e9f
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2019-10-02 20:09:39 +0900

    APIのパスにカード番号を含めるようにした

--- api/users/delete.js
+++ api/users/delete.js
@@ -1,17 +1,18 @@
 module.exports = ({ router, User, verify }) => {
-  router.delete('/', async (req, res) => {
+  router.delete('/:cardNumber', async (req, res) => {
     const idTokenString = req.token;
     const payload = await verify(idTokenString);
+    const cardNumber = req.params.cardNumber;    
+
     if (payload && payload.hd === 'esm.co.jp') {
-      const user = new User({
-        email: payload.email
-      }, req.body.cardNumber.toLocaleLowerCase());
-      const cardNumber = req.body.cardNumber.toLocaleLowerCase();
-      user.delete(cardNumber)
-        .then((response) => {
-          res.json(response);
-        })
-        .catch(err => res.status(400).json(err));
+      await User.delete(cardNumber)
+      .then((response) => {
+        res.json(response);
+      })
+      .catch(err => {
+        console.log('ERROR', err);
+        res.status(500).json(err);
+      });
 
     } else {
       res.sendStatus(403);
